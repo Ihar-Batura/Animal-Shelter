26bbe2398bdcd610854623f33607b11303778599
Ihar-Batura <A17331582@gmail.com>
2024-07-29 09:34:30 +0300

    feat: add fales from src

---
+++ shelter/src/js/index.js
@@ -0,0 +1,21 @@
+// import '../styles/style.css'
+
+// Burger navigation
+
+const nav = document.querySelector('.navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+// when someone clicks the hamburger button
+navToggle.addEventListener('click', () => {
+  const visiblity = nav.getAttribute('data-visible')
+  // if the nav is closed, open it
+  if (visiblity === 'false') {
+    nav.setAttribute('data-visible', true) // open burger
+    navToggle.setAttribute('aria-expanded', true) // change icon
+  } else {
+    nav.setAttribute('data-visible', false) // close burger
+    navToggle.setAttribute('aria-expanded', false) // change/return icon
+  }
+
+  // if the nav is open, close it
+})
